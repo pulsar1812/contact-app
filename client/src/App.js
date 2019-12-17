fccb70bf3618ac40a0539288536f6aed82320698
Patrick Lee <cluster000@gmail.com>
2019-12-17 23:00:56 +0800

    Added the client folder

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      App
+    </div>
+  );
+};
+
+export default App;
